445c9869da492966b17911f8279e42e952927dbe
RajasriVeeramusti <165761211+RajasriVeeramusti@users.noreply.github.com>
2025-04-03 12:08:03 +0530

    Time: 3 ms (48.7%), Space: 43 MB (10.82%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,27 @@
+ class Solution {
+    public String removeDuplicateLetters(String s) {
+        Stack<Character>stack=new Stack<>();
+        Set<Character>seen = new HashSet<>();
+        Map<Character, Integer> lastOccurrence = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            lastOccurrence.put(s.charAt(i),i);
+        }
+        for (int i=0; i<s.length();i++){
+            char c= s.charAt(i);
+            if(seen.contains(c)){
+                continue;
+            }
+            while(!stack.isEmpty()&& c < stack.peek()&& lastOccurrence.get(stack.peek())>i)
+            {
+                seen.remove(stack.pop());
+            }
+            stack.push(c);
+            seen.add(c);
+        }
+        StringBuilder result = new StringBuilder();
+        for(char ch : stack) {
+            result.append(ch);
+        }
+        return result.toString();
+    }
+}
